ebf6340ce87768d30ef64d44d73e8d146dd2acc8
ritasumtsova <48488494+ritasumtsova@users.noreply.github.com>
2022-05-05 10:12:29 +0300

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-// eslint-disable-next-line import/no-unresolved
 import PrivateRoute from './routes/PrivateRoute';
-// eslint-disable-next-line import/no-unresolved
 import Error from './components/pages/Error/Error';
-// eslint-disable-next-line import/no-unresolved
 import Login from './components/pages/Login/Login';
 import './App.css';
 
-// eslint-disable-next-line import/no-unresolved
 const Departments: Function = React.lazy(() => import('./components/pages/Departments/Departments'));
-// eslint-disable-next-line import/no-unresolved
 const Department: Function = React.lazy(() => import('./components/pages/Department/Department'));
 
 const App: React.FC = () => {
