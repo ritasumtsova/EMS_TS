2473883e25f4994a8689f5eff02cd91b746200e1
ritasumtsova <48488494+ritasumtsova@users.noreply.github.com>
2022-05-23 18:57:26 +0300

    removed unnecessary folder

--- src/components/selectors/authSelectors.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector } from 'react-redux';
-import { rootState } from '../../types/store/rootStateType';
-
-export const token = useSelector((state: rootState) => state.auth?.token?.token);
-export const errorMessage = useSelector((state: rootState) => state.auth?.errorMessage);
-export const loading = useSelector((state: rootState) => state.auth?.loading);
--- src/components/selectors/departmentsSelectors.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector } from 'react-redux';
-import { rootState } from '../../types/store/rootStateType';
-
-export const departments = useSelector((state: rootState) => state.departments.departments?.data);
-export const errorMessage = useSelector((state: rootState) => state.departments?.errorMessage);
-export const loading = useSelector((state: rootState) => state.departments?.loading);
